0f43d39923581c2e28bcd50540a62b66581826a3
kdh1127 <kdkdhh1127@gmail.com>
2024-07-02 14:56:52 +0900

    유니티 뱀서라이크 01,02

---
+++ Assets/Undead Survivor/Script/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        //3. 위치 이동
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}
